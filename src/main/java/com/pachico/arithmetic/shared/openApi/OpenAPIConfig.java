110025e3aa886594a6b369a32899fbcfbeb20a69
Francisco Gonzalez <franja506@gmail.com>
2023-07-21 07:24:34 -0300

    feature(mvp): refactor OpenAPIConfig

--- src/main/java/com/pachico/arithmetic/shared/openApi/OpenAPIConfig.java
+++ src/main/java/com/pachico/arithmetic/shared/openApi/OpenAPIConfig.java
@@ -1,4 +1,4 @@
-package com.pachico.arithmetic.shared;
+package com.pachico.arithmetic.shared.openApi;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.beans.factory.annotation.Value;
