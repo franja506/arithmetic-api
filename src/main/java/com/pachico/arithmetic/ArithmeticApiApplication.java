fef6488b073efac8ec84d7d75e61aef7bef0318e
Francisco Gonzalez <franja506@gmail.com>
2023-07-24 11:31:04 -0300

    feature(mvp): add retry mvp

--- src/main/java/com/pachico/arithmetic/ArithmeticApiApplication.java
+++ src/main/java/com/pachico/arithmetic/ArithmeticApiApplication.java
@@ -3,9 +3,11 @@ package com.pachico.arithmetic;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @OpenAPIDefinition
+@EnableRetry
 public class ArithmeticApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/pachico/arithmetic/adapter/out/rest/PercentageClient.java
+++ src/main/java/com/pachico/arithmetic/adapter/out/rest/PercentageClient.java
@@ -3,11 +3,14 @@ package com.pachico.arithmetic.adapter.out.rest;
 import com.pachico.arithmetic.adapter.out.rest.model.PercentageRequest;
 import com.pachico.arithmetic.adapter.out.rest.model.PercentageResponse;
 import com.pachico.arithmetic.application.port.out.RetrievePercentagePortOut;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
@@ -17,6 +20,7 @@ import java.math.BigDecimal;
 import static java.math.RoundingMode.HALF_UP;
 
 @Component
+@Slf4j
 public class PercentageClient implements RetrievePercentagePortOut {
 
     private final RestTemplate restTemplate;
@@ -24,24 +28,31 @@ public class PercentageClient implements RetrievePercentagePortOut {
     @Value("${franjagonca.rest.base-url}")
     private String base_url;
 
-    private final String PATH = "/percentage";
-
     @Autowired
     public PercentageClient(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     @Override
+    @Retryable(backoff = @Backoff(delay = 1000), retryFor = RuntimeException.class)
     public BigDecimal execute(BigDecimal x, BigDecimal y) {
+       BigDecimal result;
+       try {
+           val entity = new HttpEntity<>(createRequest(x,y),getHeaders(x,y));
 
-        val entity = new HttpEntity<>(createRequest(x,y),getHeaders(x,y));
+           val percentage = restTemplate.postForObject(retrieveURL(),entity, PercentageResponse.class);
 
-        val percentage = restTemplate.postForObject(retrieveURL(),entity, PercentageResponse.class);
+           result =  percentage.getPercentage();
+       } catch (Exception e) {
+           log.error("Hubo un error invocando la api percentage");
+           throw new RuntimeException();
+       }
 
-        return percentage.getPercentage();
+       return result;
     }
 
     private String retrieveURL() {
+        String PATH = "/percentage";
         return base_url + PATH;
     }
 
