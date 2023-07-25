ed9ef53718ce79b7d55ea5539036803c14eecca8
Francisco Gonzalez <franja506@gmail.com>
2023-07-25 19:04:53 -0300

    feature(mvp): add percentageClient test

--- src/test/java/com/pachico/arithmetic/adapter/out/rest/PercentageClientTest.java
+++ src/test/java/com/pachico/arithmetic/adapter/out/rest/PercentageClientTest.java
@@ -8,16 +8,15 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-// TODO: Terminar este test
+
 public class PercentageClientTest {
 
     @Mock
@@ -38,35 +37,17 @@ public class PercentageClientTest {
         BigDecimal y = BigDecimal.valueOf(20);
         BigDecimal mockPercentage = SharedStubs.percentage;
 
-        // Mock the RestTemplate behavior
-        when(mockRestTemplate.postForObject(any(), any(), any())).thenReturn(WebStubs.getPercentageResponse());
+        // Given
+        when(mockRestTemplate.postForObject(anyString(), any(HttpEntity.class), eq(PercentageResponse.class)))
+                .thenReturn(WebStubs.getPercentageResponse());
 
-        // Call the execute method
+        // Then
         BigDecimal result = percentageClient.execute(x, y);
 
-        // Verify that the RestTemplate.postForObject method was called with the expected arguments
-        verify(mockRestTemplate).postForObject(any(), any(), eq(PercentageResponse.class));
-
-        // Verify the result
+        // When
         assertEquals(mockPercentage, result);
 
-        // Verify that lastPercentage was set correctly
-        //assertEquals(mockPercentage, percentageClient);
-    }
+        verify(mockRestTemplate).postForObject(anyString(), any(HttpEntity.class), eq(PercentageResponse.class));
 
-    @Test
-    public void testExecute_Failure() {
-        // Prepare the test data
-        BigDecimal x = BigDecimal.valueOf(10);
-        BigDecimal y = BigDecimal.valueOf(20);
-
-        // Mock the RestTemplate behavior to throw an exception
-        when(mockRestTemplate.postForObject(any(), any(), any())).thenThrow(new RuntimeException("Some error"));
-
-        // Call the execute method and expect a RuntimeException
-        assertThrows(RuntimeException.class, () -> percentageClient.execute(x, y));
-
-        // Verify that lastPercentage remains unchanged
-        //assertEquals(null, percentageClient.getLastPercentage());
     }
 }
